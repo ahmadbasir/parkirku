53bb99cfb6740e44aa1bd72968b5949186d25fb7
cipow <cipoghost@gmail.com>
2017-03-30 11:42:43 +0700

    Isi panduan selesai

--- Laravel/resources/views/panelAdmin/layanan/panduan.blade.php
+++ Laravel/resources/views/panelAdmin/layanan/panduan.blade.php
@@ -22,25 +22,6 @@
   		<div class="timeline-items">
   			<div class="timeline-item clearfix">
   				<div class="timeline-info">
-  					<span class="label label-success label-sm">#*</span>
-  				</div>
-  				<div class="widget-box transparent">
-  					<div class="widget-header widget-header-small">
-  						<h5 class="widget-title smaller">
-  							<span class="grey">Markir</span>
-  						</h5>
-  						<span class="widget-toolbar no-border">
-  							<a href="#" class="green">
-  								<i class="ace-icon glyphicon glyphicon-send"></i><b> Go</b>
-  							</a>
-  						</span>
-  					</div>
-  				</div>
-  			</div>
-  		</div>
-  		<div class="timeline-items">
-  			<div class="timeline-item clearfix">
-  				<div class="timeline-info">
   					<span class="label label-danger label-sm"><b>#1</b></span>
   				</div>
   				<div class="widget-box transparent">
@@ -49,7 +30,7 @@
 							<span class="grey">Kendaraan Masuk</span>
   						</h5>
   						<span class="widget-toolbar no-border">
-							<a href="#" class="red">
+							<a href="#KM" class="red">
 								<i class="ace-icon glyphicon glyphicon-send"></i><b> Go</b>
 							</a>
   						</span>
@@ -69,7 +50,7 @@
   							<span class="grey">Daftar Kendaraan</span>
   						</h5>
   						<span class="widget-toolbar no-border">
-  							<a href="#" style="color: #f89406;">
+  							<a href="#DK" style="color: #f89406;">
   								<i class="ace-icon glyphicon glyphicon-send"></i><b> Go</b>
   							</a>
   						</span>
@@ -85,10 +66,29 @@
   				<div class="widget-box transparent">
   					<div class="widget-header widget-header-small">
   						<h5 class="widget-title smaller">
-  							<span class="grey">Lapor</span>
+  							<span class="grey">Kendaraan Keluar</span>
   						</h5>
   						<span class="widget-toolbar no-border">
-  							<a href="#" class="blue">
+  							<a href="#KK" class="blue">
+  								<i class="ace-icon glyphicon glyphicon-send"></i><b> Go</b>
+  							</a>
+  						</span>
+  					</div>
+  				</div>
+  			</div>
+  		</div>
+      <div class="timeline-items">
+  			<div class="timeline-item clearfix">
+  				<div class="timeline-info">
+  					<span class="label label-success label-sm">#4</span>
+  				</div>
+  				<div class="widget-box transparent">
+  					<div class="widget-header widget-header-small">
+  						<h5 class="widget-title smaller">
+  							<span class="grey">Lapor !!!</span>
+  						</h5>
+  						<span class="widget-toolbar no-border">
+  							<a href="#L" class="green">
   								<i class="ace-icon glyphicon glyphicon-send"></i><b> Go</b>
   							</a>
   						</span>
@@ -110,7 +110,7 @@
   				</div>
   				<div class="widget-box transparent">
   					<div class="widget-header widget-header-small">
-  						<h5 class="widget-title smaller"><b>Kendaraan Masuk</b></h5>
+  						<h5 id="KM" class="widget-title smaller"><b>Kendaraan Masuk</b></h5>
   						<span class="widget-toolbar no-border">
   								<i class="ace-icon fa fa-check blue"></i>
   						</span>
@@ -154,7 +154,7 @@
   				</div>
   				<div class="widget-box transparent">
   					<div class="widget-header widget-header-small">
-  						<h5 class="widget-title smaller"><b>Daftar Kendaraan</b></h5>
+  						<h5 id="DK" class="widget-title smaller"><b>Daftar Kendaraan</b></h5>
   						<span class="widget-toolbar no-border">
   								<i class="ace-icon fa fa-check blue"></i>
   						</span>
@@ -174,8 +174,73 @@
                   jika icon ditekan dan statusnya sudah keluar <img class="img-rounded img-responsive" src="{{asset('assets/images/icon_ok.png')}}" alt="icon" />
                   <br>
                   <div class="alert alert-info">
-                    Note: icon tidak akan muncul pada tampilan Desktop.
+                    Note: icon tidak akan muncul pada tampilan Desktop. Karena akan langsung ditampilkan semua
                   </div>
+                </p>
+  						</div>
+  					</div>
+  				</div>
+  			</div>
+  		</div>
+      <div class="timeline-items">
+  			<div class="timeline-item clearfix">
+  				<div class="timeline-info">
+  					<i class="timeline-indicator ace-icon btn no-hover glyphicon glyphicon-tags"></i>
+  				</div>
+  				<div class="widget-box transparent">
+  					<div class="widget-header widget-header-small">
+  						<h5 id="KK" class="widget-title smaller"><b>Kendaraan Keluar</b></h5>
+  						<span class="widget-toolbar no-border">
+  								<i class="ace-icon fa fa-check blue"></i>
+  						</span>
+  					</div>
+  					<div class="widget-body">
+  						<div class="widget-main">
+  							<p>
+                  Masih pada menu Daftar Kendaraan, pada tampilan daftar kendaraan
+                  terdapat icon pencarian.
+                  <img class="img-rounded img-responsive" src="{{asset('assets/images/cari.png')}}" alt="cari" />
+                  <h3>#petunjuk kendaraan akan keluar</h3><br>
+                  <ol type="1">
+                    <li>masukkan no plat pada icon pencarian</li>
+                    <li>tekan tombol icon berwarna kuning <img class="img-rounded img-responsive" src="{{asset('assets/images/icon.png')}}" alt="icon" /></li>
+                    <li>kemudian tekan tanda <img class="img-rounded img-responsive" src="{{asset('assets/images/icon_x.png')}}" alt="icon" /></li>
+                    <li>maka otomatis status kendaraan akan sudah keluar dan jam keluarnya</li>
+                  </ol>
+                  <br><br>
+                  <div class="alert alert-info">
+                    <img class="img-rounded img-responsive" src="{{asset('assets/images/icon_edit.png')}}" alt="icon" />
+                    Icon ini berfungsi untuk mengedit informasi kendaraan misal jika ada kesalahan pada penginputa,
+                    tampilannya mirip dengan form kendaraan masuk.
+                  </div>
+                </p>
+  						</div>
+  					</div>
+  				</div>
+  			</div>
+  		</div>
+      <div class="timeline-items">
+  			<div class="timeline-item clearfix">
+  				<div class="timeline-info">
+  					<i class="timeline-indicator ace-icon btn no-hover glyphicon glyphicon-tags"></i>
+  				</div>
+  				<div class="widget-box transparent">
+  					<div class="widget-header widget-header-small">
+  						<h5 id="L" class="widget-title smaller"><b>Lapor !!!</b></h5>
+  						<span class="widget-toolbar no-border">
+  								<i class="ace-icon fa fa-check blue"></i>
+  						</span>
+  					</div>
+  					<div class="widget-body">
+  						<div class="widget-main">
+  							<p>
+                  Pada menu ini akan menampilkan daftar Lapor dari user pengguna aplikasi
+                  <br>
+                  <img class="img-rounded img-responsive" src="{{asset('assets/images/lapor.png')}}" alt="icon" />
+                  <br><br>
+                  Pada gambar di atas terlihat bagian informasi judul, nama pelapor, pesan, waktu lapor, dan nomor yang dapat dihubungi.<br>
+                  Sebagai petugas parkir anda diwajibkan untuk mengecek menu ini, jika ada laporan kehilangan atau lainnya. <br>
+                  Dan jika telah menemukan solusi untuk pelapor atau permintaan dari pelapor, anda bisa menghubungi nomor yang telah dikirimkan.
                 </p>
   						</div>
   					</div>
