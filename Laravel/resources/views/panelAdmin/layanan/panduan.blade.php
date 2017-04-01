3ddbe55995ce92900de88298171f3fc3a0c8a23b
ahmad5basir <ahmad5basir@gmail.com>
2017-04-01 16:10:32 +0700

    @panduan, Done!

--- Laravel/resources/views/panelAdmin/layanan/panduan.blade.php
+++ Laravel/resources/views/panelAdmin/layanan/panduan.blade.php
@@ -225,25 +225,31 @@
   						</span>
   					</div>
   					<div class="widget-body">
-  						<div class="widget-main">
-  							<p>
+  						<div class="widget-main col-xs-12 col-sm-4">
+                  <span class="label-lg alert-info"><b>1.</b></span> 
                   Masih pada menu Daftar Kendaraan, pada tampilan daftar kendaraan
                   terdapat icon pencarian.
+                  <br>
                   <img class="img-rounded img-responsive" src="{{asset('assets/images/cari.png')}}" alt="cari" />
-                  <h3>#petunjuk kendaraan akan keluar</h3><br>
+                  
+                  <h3 class="alert alert-info">#petunjuk kendaraan akan keluar</h3>
                   <ol type="1">
                     <li>masukkan no plat pada icon pencarian</li>
                     <li>tekan tombol icon berwarna kuning <img class="img-rounded img-responsive" src="{{asset('assets/images/icon.png')}}" alt="icon" /></li>
                     <li>kemudian tekan tanda <img class="img-rounded img-responsive" src="{{asset('assets/images/icon_x.png')}}" alt="icon" /></li>
                     <li>maka otomatis status kendaraan akan sudah keluar dan jam keluarnya</li>
                   </ol>
-                  <br><br>
+              </div>
+              <div class="widget-main col-xs-12 col-sm-8">
                   <div class="alert alert-info">
+                    <i class="ace-icon fa fa-quote-left"></i>
+                    <p style="text-align: justify;">
+                      untuk mengedit informasi kendaraan misal jika ada kesalahan pada penginputan, tampilannya mirip dengan form kendaraan masuk. yaitu dengan mengklick icon dibawah ini, kemudia akan muncul menu edit.
+                    </p>
                     <img class="img-rounded img-responsive" src="{{asset('assets/images/icon_edit.png')}}" alt="icon" />
-                    Icon ini berfungsi untuk mengedit informasi kendaraan misal jika ada kesalahan pada penginputa,
-                    tampilannya mirip dengan form kendaraan masuk.
+                    <i class="ace-icon fa fa-quote-right pull-right"></i>
+                    ____
                   </div>
-                </p>
   						</div>
   					</div>
   				</div>
@@ -252,26 +258,35 @@
       <div class="timeline-items">
   			<div class="timeline-item clearfix">
   				<div class="timeline-info">
-  					<i class="timeline-indicator ace-icon btn no-hover glyphicon glyphicon-tags"></i>
+  					<i class="timeline-indicator ace-icon btn btn-success no-hover glyphicon glyphicon-tags"></i>
   				</div>
   				<div class="widget-box transparent">
-  					<div class="widget-header widget-header-small">
-  						<h5 id="L" class="widget-title smaller"><b>Lapor !!!</b></h5>
+  					<div class="widget-header widget-header-small" style="background-color: #87B87F;">
+  						<h5 id="L" class="widget-title smaller white"><b>Lapor !!!</b></h5>
   						<span class="widget-toolbar no-border">
-  								<i class="ace-icon fa fa-check blue"></i>
+  								<i class="ace-icon fa fa-check white"></i>
   						</span>
   					</div>
   					<div class="widget-body">
-  						<div class="widget-main col-xs-12 col-sm-6">
-                
-                  <img class="img-rounded img-responsive" src="{{asset('assets/images/lapor.png')}}" alt="icon" />
-                <p>
+              <div class="widget-main col-sm-5 col-xs-12">
+                  <img class="img-rounded img-responsive" src="{{asset('assets/images/lapor.png')}}" alt="icon"/>
+              </div>
+              <div class="widget-main col-xs-12 col-sm-7">
+                <p class="alert alert-success" style="text-align: justify;">
+                  <i class="ace-icon fa fa-quote-right"></i>
                   Pada menu ini akan menampilkan daftar Lapor dari user pengguna aplikasi
-                  <br>
-                  <br><br>
-                  Pada gambar di atas terlihat bagian informasi judul, nama pelapor, pesan, waktu lapor, dan nomor yang dapat dihubungi.<br>
-                  Sebagai petugas parkir anda diwajibkan untuk mengecek menu ini, jika ada laporan kehilangan atau lainnya. <br>
-                  Dan jika telah menemukan solusi untuk pelapor atau permintaan dari pelapor, anda bisa menghubungi nomor yang telah dikirimkan.
+                </p>
+                  Pada gambar di atas terlihat bagian informasi :
+                <ul>
+                  <li>judul</li>
+                  <li>nama pelapor</li>
+                  <li>pesan</li>
+                  <li>waktu lapor</li>
+                  <li>nomor yang dapat dihubungi</li>
+                </ul>
+                <p style="text-align: justify;" class="alert alert-success">
+                  <i class="ace-icon fa fa-quote-right"></i>
+                  Sebagai petugas parkir anda diwajibkan untuk mengecek menu ini, jika ada laporan kehilangan atau lainnya. Dan jika telah menemukan solusi untuk pelapor atau permintaan dari pelapor, anda bisa menghubungi nomor yang telah dikirimkan.
                 </p>
   						</div>
   					</div>
