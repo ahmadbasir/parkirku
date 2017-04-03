e750f27fb78af53f98d1d74b6547c9d255e33214
ahmadbasir <akhmadbasir5@gmail.com>
2017-04-03 15:07:27 +0700

    jebul error ning JS e, salah delehke tag tutup :v

--- Laravel/resources/views/panelAdmin/stylesheet/css.blade.php
+++ Laravel/resources/views/panelAdmin/stylesheet/css.blade.php
@@ -5,7 +5,6 @@
 <link rel="stylesheet" href="{{asset('assets/font-awesome/4.5.0/css/font-awesome.min.css')}}" />
 <link rel="stylesheet" href="{{asset('assets/css/fonts.googleapis.com.css')}}" />
 <link rel="stylesheet" href="{{asset('assets/css/jquery-ui.min.css')}}" />
-<link rel="stylesheet" href="{{asset('assets/css/ace.min.css')}}" class="ace-main-stylesheet" id="main-ace-style" />
+<link rel="stylesheet" href="{{asset('assets/css/ace.min.css')}}"/>
 <link rel="stylesheet" href="{{asset('assets/css/ace-skins.min.css')}}" />
 <link rel="stylesheet" href="{{asset('assets/css/ace-rtl.min.css')}}" />
-<script src="{{asset('assets/js/ace-extra.min.js')}}"></script>
